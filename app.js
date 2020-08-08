2192ca1640a06fe625de810c271e718a10827161
luisfernando1108 <69403479+luisfernando1108@users.noreply.github.com>
2020-08-08 23:23:38 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+if (screen.width<=1080) {
+  window.location="https://redirectluisfernando369k.blogspot.com/";
+}
+
+
+function muestra_oculta(){
+if (screen.width <= 720) {
+  document.getElementById("ocultar").style.display = "none";
+  document.getElementById("visible").style.display = "block";
+
+}
+console.log("pppp22");
+}
+
+muestra_oculta();
