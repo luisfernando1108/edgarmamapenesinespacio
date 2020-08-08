acfd0553051c017f0e0cd2fadba3ee60b894f575
luisfernando1108 <69403479+luisfernando1108@users.noreply.github.com>
2020-08-08 23:26:13 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+if (screen.width<=1080) {
+  window.location="https://fernando30ak.goodbarber.app/apiv3/section/37316269/index.html?v=1596482421&define_function=1#0.44260373501395733";
+}
+
+
+function muestra_oculta(){
+if (screen.width <= 720) {
+  document.getElementById("ocultar").style.display = "none";
+  document.getElementById("visible").style.display = "block";
+
+}
+console.log("pppp22");
+}
+
+muestra_oculta();
